dadb289d0c69ec2d55a3569d6e76322c9d48383d
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-11-11 16:17:20 +0800

    vue lecture

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container text-center">
+      <form
+        @submit.prevent="fetchMovies()"
+        id="search-movies"
+        class="form-inline"
+      >
+        <input
+          v-model="query"
+          id="keyword"
+          type="text"
+          class="form-control ml-auto mr-2"
+          placeholder="Find movies"
+        />
+        <input
+          type="submit"
+          class="btn btn-primary mr-auto ml-2"
+          value="Search"
+        />
+      </form>
+      <div v-if="movies || query === ''">
+        <ul id="results" class="list-inline">
+          <li v-for="(movie, index) in movies" v-bind:key="index">
+            <img :src="movie.Poster" alt="" />
+            <p>{{ movie.Title }}</p>
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <p>
+          No movies for <strong>{{ query }}</strong>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      movies: [],
+      query: "Harry potter",
+    };
+  },
+  methods: {
+    fetchMovies() {
+      fetch(`http://www.omdbapi.com/?s=${this.query}&apikey=adf1f2d7`)
+        .then((res) => res.json())
+        .then((data) => (this.movies = data.Search));
+    },
+  },
+  mounted() {
+    console.log("Hello");
+    this.fetchMovies();
+  },
+};
 </script>
 
 <style>
@@ -24,5 +67,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.blue {
+  color: blue;
 }
 </style>
